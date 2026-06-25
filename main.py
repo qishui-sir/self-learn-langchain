310c2d82b333b78dd77b75a3cba3459fb1a275e6
1623625212 <1623625212@qq.com>
2026-06-25 22:31:10 +0800

    feat:messages and markdown

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from learning!")
+from langchain.agents import create_agent
+from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+import pydantic 
+
+load_dotenv()
+
+system_promt ="""
+#身份
+- 你是一个ai编程助手，擅长用企业级的代码编写程序
+
+#指令
+- 用snake case命名，不要用camel case命名法
+-  不要返回markdwn格式说明，只需要返回代码
+
+#示例
 
 
-if __name__ == "__main__":
-    main()
+"""
+
+agent = create_agent(
+    model="deepseek-chat",
+    system_prompt=system_promt,
+)
+
+human_messages = HumanMessage(content="用python写一个简洁的贪心算法")
+
+messages = agent.stream(
+    {"messages":human_messages},
+    stream_mode="messages",
+)
+for token,metadata in messages:
+    if token:
+        print(token.content,end="",flush=True)
