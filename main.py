8dc5a02b74619070553807e0f08a3154ca6c6cc8
qishui-sir <1623625212@qq.com>
2026-06-26 15:48:05 +0800

    fear:add tool and short memory

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from langchain.agents import create_agent
+from langchain.tools import tool
 from dotenv import load_dotenv
 from langchain_core.messages import HumanMessage
-import pydantic 
+from langchain_tavily import TavilySearch
+from langgraph.checkpoint.memory import InMemorySaver
 
 load_dotenv()
 
@@ -18,17 +20,33 @@ system_promt ="""
 
 """
 
+Search_Tavily = TavilySearch(
+    max_result = 5,
+    topic = "general",
+)
+
 agent = create_agent(
     model="deepseek-chat",
     system_prompt=system_promt,
+    tools=[Search_Tavily],
+    checkpointer=InMemorySaver(),
 )
 
-human_messages = HumanMessage(content="用python写一个简洁的贪心算法")
+config = {"configurable":{"thread_id":"1"}}
 
-messages = agent.stream(
+human_messages = HumanMessage(content="我最喜欢猫咪")
+response = agent.invoke(
     {"messages":human_messages},
-    stream_mode="messages",
+    config,
 )
-for token,metadata in messages:
-    if token:
-        print(token.content,end="",flush=True)
+
+human_messages = HumanMessage(content="你知道我最喜欢什么动物吗")
+response = agent.invoke(
+    {"messages":human_messages},
+    config,
+)
+print(response["messages"][-1].content+"\n")
+
+def Search_Tavly(query: str):
+    """Search web for the imformation"""
+    return Search_Tavily.invoke(query)
